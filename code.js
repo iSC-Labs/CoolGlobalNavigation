9ba8a077ce6bed4cd544b3bacf49188daae214bb
Paynekiller92 <paynekiller92@outlook.com>
2015-02-23 21:54:17 +0100

    Update code.js

--- code.js
+++ code.js
@@ -2,4 +2,4 @@
 // JS to set the 'inverse' class
 document.getElementById("globalNavigation").className = "global-navigation inverse"; 
 // JQuery to load CSS
-$('head').append('<link rel="stylesheet" href="//comunidad.wikia.com/wiki/User:Paynekiller92/wikia.css?action=raw&ctype=css" type="text/css" />');
+$('head').append('<link rel="stylesheet" href="//raw.githubusercontent.com/Paynekiller92/CoolGlobalNavigation/master/code.js" type="text/css" />');
